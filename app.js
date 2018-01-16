0ab74ef06646ae6d838d2de24f91cded65b4c8b6
Héctor Villaverde <hvilla65@gmail.com>
2018-01-15 18:24:13 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -2,32 +2,43 @@
 
 var express = require( 'express' );
 var mongoose = require( 'mongoose' );
+var bodyParser = require ( 'body-parser' );
 
 // Inicializar variables
 
 var app = express();
 const colorVerde = '\x1b[32m%s\x1b[0m';
 
+
+// body-parser
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importar rutas
+
+var appRoutes = require( './routes/app' );
+var usuarioRoutes = require( './routes/usuario' );
+var loginRoutes = require( './routes/login' );
+
 // Conexión a la base de datos
 
 mongoose.connection.openUri( 'mongodb://localhost:27017/hospitalDB', ( err, res ) => {
     if ( err ) throw err;
-    
     console.log( `Base de datos: ${ colorVerde }` , 'online' )
-
-} );
+});
 
 // Rutas
 
-app.get( '/', ( req, res, next ) => {
-    res.status( 200 ).json( {
-        ok: true,
-        mensaje: 'Petición realizada correctamente, probando nodemon'
-    })
-})
+app.use( '/usuario', usuarioRoutes );
+app.use( '/login', loginRoutes );
+app.use( '/', appRoutes );
 
 // Escuchar peticiones
 
-app.listen( 3000, () => {
-    console.log( `Express server corriendo en el puerto 3000: ${ colorVerde }` , 'online' )
+console.log( 'entrando');
+
+app.listen( 3000, ( ) => {
+    console.log( `Express server corriendo en el puerto 3000: ${colorVerde}` , 'online' )
 } );
